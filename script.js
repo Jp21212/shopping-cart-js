c9590ad16a6ed3622d6a5a188db633b156b5196a
Jp21212 <Jamesp2814@tuckahoeschools.org>
2024-11-21 13:32:13 +0000

    Initial commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let shoppingCart = []; //Creates Array for Shopping cart
 let appleButton = document.querySelector('#add-apple'); //gets apple button
 let cartList = document.querySelector('#cart-items');  // gets cart ul
+let blueberryButton = document.querySelector('#add-blueberry'); //gets blueberry button
+let kiwiButton = document.querySelector('#add-kiwi'); //gets kiwi button
+
 
 console.log(shoppingCart);
 
@@ -8,5 +11,23 @@ appleButton.addEventListener("click", ()=>{
     shoppingCart.push("Apple");
     let li = document.createElement("li");
     li.textContent= "Apple";
+    cartList.appendChild(li);
+});
+
+
+
+kiwiButton.addEventListener("click", ()=>{
+    shoppingCart.push("Kiwi");
+    let li = document.createElement("li");
+    li.textContent= "Kiwi";
+    cartList.appendChild(li);
+});
+
+
+
+blueberryButton.addEventListener("click", ()=>{
+    shoppingCart.push("Blueberry");
+    let li = document.createElement("li");
+    li.textContent= "Blueberry";
     cartList.appendChild(li);
 });
